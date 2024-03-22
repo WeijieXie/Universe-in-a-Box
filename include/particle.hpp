10b6c037b1e4d639b1cc460e150984fd854b4fde
dan <xwj.harry@gmail.com>
2024-03-22 13:48:15 +0000

    make the initialization of static member varaible inline

--- include/particle.hpp
+++ include/particle.hpp
@@ -6,18 +6,19 @@
 class particle
 {
 private:
-    static double mass;
+    static inline double mass = 1.0;
 
 public:
     std::vector<double> position = {0.0, 0.0, 0.0};
     std::vector<double> velocity = {0.0, 0.0, 0.0};
     // std::vector<double> acceleration = {0.0, 0.0, 0.0};
 
-    particle()=default;
+    particle() = default;
     particle(std::vector<double>);
 
+    static double massGetter();
     static void massSetter(double mass);
     void positionSetter(std::vector<double> positionNew);
 };
 
-double particle::mass = 1.0;
+// double particle::mass = 1.0;
