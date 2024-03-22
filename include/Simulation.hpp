219d83de783ef450bb04f8d7e64e2c4624100488
dan <xwj.harry@gmail.com>
2024-03-22 20:50:34 +0000

    implement the potential calculator

--- include/Simulation.hpp
+++ include/Simulation.hpp
@@ -10,7 +10,6 @@ class Simulation
 public:
     double timeMax;
     double timeStep;
-    particles particlesSimu;
     double width;
     int numOfCellsPerDim;
     double expanFac;
@@ -20,6 +19,12 @@ public:
     double volOfCell;
     double relCellWidth; // which is 1.0/numOfCellsPerDim
     double densityContributionPerParticle;
+    double wSquare;
+    std::vector<double> kSquare;
+    const double PI = 3.141592653589793238463;
+    double fNorm;
+
+    particles particlesSimu;
 
     fftw_complex *densityBuffer = nullptr;
     fftw_complex *potentialBuffer = nullptr;
@@ -32,6 +37,7 @@ public:
     ~Simulation();
 
     void densityCalculator();
+    void potentialCalculator();
 
     /**
      * @brief Run a particle mesh simulation from t=0 to t_max
--- lib/Simulation.cpp
+++ lib/Simulation.cpp
@@ -15,13 +15,28 @@ Simulation::Simulation(double timeMax, double timeStep, particles initParticles,
     this->volOfCell = this->volOfBox / this->numOfCells;
     this->relCellWidth = 1.0 / this->numOfCellsPerDim;
     this->densityContributionPerParticle = particle::massGetter() / this->volOfCell; // need updating per iteration
+    this->wSquare = this->width * this->width;
+    this->kSquare = std::vector<double>(this->numOfCells);
+    int index = 0;
+    for (int i = 0; i < this->numOfCellsPerDim; i++)
+    {
+        for (int j = 0; j < this->numOfCellsPerDim; j++)
+        {
+            for (int k = 0; k < this->numOfCellsPerDim; k++)
+            {
+                index = i * pow(this->numOfCellsPerDim, 2) + j * this->numOfCellsPerDim + k;
+                this->kSquare[index] = (k * k + j * j + i * i) / this->wSquare;
+            }
+        }
+    }
+    this->fNorm = 1.0 / (8 * this->numOfCells);
 
     this->densityBuffer = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * this->numOfCells);
     this->potentialBuffer = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * this->numOfCells);
     this->frequencyBuffer = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * this->numOfCells);
 
     this->forward_plan = fftw_plan_dft_3d(this->numOfCellsPerDim, this->numOfCellsPerDim, this->numOfCellsPerDim, this->densityBuffer, this->frequencyBuffer, FFTW_FORWARD, FFTW_MEASURE);
-    this->inverse_plan = fftw_plan_dft_3d(this->numOfCellsPerDim, this->numOfCellsPerDim, this->numOfCellsPerDim, this->frequencyBuffer, this->potentialBuffer, FFTW_FORWARD, FFTW_MEASURE);
+    this->inverse_plan = fftw_plan_dft_3d(this->numOfCellsPerDim, this->numOfCellsPerDim, this->numOfCellsPerDim, this->frequencyBuffer, this->potentialBuffer, FFTW_BACKWARD, FFTW_MEASURE);
 }
 
 Simulation::~Simulation()
@@ -51,10 +66,31 @@ void Simulation::densityCalculator()
         for (int i = 0; i < 3; ++i)
         {
             std::modf(iter->position[i] / this->relCellWidth, &intePart); // get the integer part from the division to get the index of each coordinates of the cell
-            index += intePart * pow(this->numOfCellsPerDim, i);           // get the index of the cell in the 1-d array
+            index += intePart * pow(this->numOfCellsPerDim, 2 - i);       // get the index of the cell in the 1-d array
         }
         this->densityBuffer[index][0] += this->densityContributionPerParticle; // add the density to the desity buffer representing the cell
     }
+}
+
+void Simulation::potentialCalculator()
+{
+    fftw_execute(this->forward_plan);
+
+    this->frequencyBuffer[0][0] = 0.0;
+    this->frequencyBuffer[0][1] = 0.0;
+    for (int i = 1; i < this->numOfCells; i++)
+    {
+        this->frequencyBuffer[i][0] *= -4 * this->PI / kSquare[i] * this->fNorm;
+        this->frequencyBuffer[i][1] *= -4 * this->PI / kSquare[i] * this->fNorm;
+    }
+
+    fftw_execute(this->inverse_plan);
+
+    // for (int i = 0; i < this->numOfCells; i++)
+    // {
+    //     this->potentialBuffer[i][0] *= this->fNorm;
+    //     this->potentialBuffer[i][1] *= this->fNorm;
+    // }
 }
 
 void Simulation::run()
