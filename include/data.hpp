46a9dc9f820f15228714a6250c47b9f59c932682
dan <xwj.harry@gmail.com>
2024-03-22 18:22:57 +0000

    add test data

--- include/data.hpp
+++ include/data.hpp
@@ -5,7 +5,20 @@
 class test_data
 {
 public:
+    static const double timeMax;
+    static const double timeStep;
+    static const double mass;
+    static const double width;
+    static const double expanFac;
+
+    static const int numOfCellsPerDim;
+    static const double densityContributionPerParticle;
+
     static const particles initParticlesEmpty;
     static const particles initParticlesRandom;
     static const particles initParticlesOneInMid;
+    static const particles initParticlesMultiple;
+
+    static const fftw_complex densityBufferForSingleParticle[8];
+    static const fftw_complex densityBufferForMultipleParticles[8];
 };
