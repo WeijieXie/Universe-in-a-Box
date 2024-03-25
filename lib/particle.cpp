5d4dd370401d71d7001b50d902229855cdfe7fb8
dan <xwj.harry@gmail.com>
2024-03-25 23:21:58 +0000

    add alternative way to implement the method of updater

--- lib/particle.cpp
+++ lib/particle.cpp
@@ -25,12 +25,19 @@ void particle::positionSetter(std::vector<double> positionNew)
     double intePart;
     for (int i = 0; i < 3; ++i)
     {
-        if (positionNew[i] < 0)
+        if (positionNew[i] < 0.0)
         {
-            position[i] = std::modf(positionNew[i], &intePart) + 1;
+            // std::cout << "smaller than 0.0 ......................................" << std::endl;
+            position[i] = std::modf(positionNew[i], &intePart) + 1.0;
+            // if (position[i] == 1.0)
+            // {
+            //     position[i] = 0.0;
+            //     std::cout << "................................................................................." << std::endl;
+            // }
         }
-        else if (positionNew[i] >= 1)
+        else if (positionNew[i] >= 1.0)
         {
+            // std::cout << "equal or larger than 1.0 ......................................" << std::endl;
             position[i] = std::modf(positionNew[i], &intePart);
         }
         else
@@ -38,15 +45,18 @@ void particle::positionSetter(std::vector<double> positionNew)
             position[i] = positionNew[i];
         }
     }
+    // std::cout << "input: " << positionNew[0] << " " << positionNew[1] << " " << positionNew[2] << std::endl;
+    // std::cout << "posi: " << position[0] << " " << position[1] << " " << position[2] << std::endl;
 }
 
 void particle::updater(std::vector<double> acceleration)
 {
-
+    // std::vector<double> positionNew;
     for (int i = 0; i < 3; i++)
     {
         this->velocity[i] += acceleration[i] * this->dt;
         this->position[i] += this->velocity[i] * this->dt;
+        // positionNew.push_back(this->position[i] + this->velocity[i] * this->dt);
     }
     positionSetter(this->position);
 }
