1f71dcb47dc20e62bcfcd743c4ecf917d8d20445
dan <xwj.harry@gmail.com>
2024-03-28 23:03:38 +0000

    add tests for accelerationCalculator and particlesUpdater

--- test/test_simulation.cpp
+++ test/test_simulation.cpp
@@ -5,6 +5,27 @@
 
 using namespace Catch::Matchers;
 
+TEST_CASE("Test simulation constructor", "Constructor Tests")
+{
+    // Declare simulation with particle setup, width, and number of cells.
+    // TODO: Simulation sim(...);
+
+    int numOfCellsPerDim = 101;
+    int numOfParticlesPerCell = 10;
+    double timeMax = 1.5;
+    double timeStep = 0.01;
+    double expanFac = 1.0;
+    std::optional<std::string> outputFolder = std::nullopt;
+    int seed = 23093556;
+
+    int numOfParticles = pow(numOfCellsPerDim, 3) * numOfParticlesPerCell;
+    particles initParticles = particles(numOfParticles, seed);
+    particle::massSetter(100000.0 / numOfParticlesPerCell);
+    Simulation simulation(timeMax, timeStep, initParticles, 100.0, numOfCellsPerDim, expanFac);
+
+    REQUIRE(simulation.particlesSimu.particleInfo.size() == numOfParticles);
+}
+
 TEST_CASE("Test the imaginary part of density to be 0.0", "Density Tests")
 {
     // One particle in the centre of the box with mass 0.01
@@ -26,7 +47,6 @@ TEST_CASE("Test the imaginary part of density to be 0.0", "Density Tests")
         size++;
     }
     REQUIRE(size == 1030301);
-    // std::cout<<"size: "<<size<<std::endl;
 
     // Look at the density buffer
     for (auto iter = simulation.densityBuffer; iter < simulation.densityBuffer + simulation.numOfCells; iter++)
@@ -84,12 +104,10 @@ TEST_CASE("Test density of single particle", "Density Tests")
         size++;
     }
     REQUIRE(size == 8);
-    // std::cout<<"size: "<<size<<std::endl;
 
     // Look at the density buffer
     for (int i = 0; i < simulation.numOfCells; i++)
     {
-        // std::cout<<i<<": "<< simulation.densityBuffer[i][0]<<", "<< simulation.densityBuffer[i][1]<<std::endl;;
         REQUIRE(simulation.densityBuffer[i][0] == test_data::densityBufferForSingleParticle[i][0]);
         REQUIRE(simulation.densityBuffer[i][1] == 0.0);
     }
@@ -113,16 +131,15 @@ TEST_CASE("Test density of multiple particles", "Density Tests")
         size++;
     }
     REQUIRE(size == 8);
-    // std::cout<<"size: "<<size<<std::endl;
 
     // Look at the density buffer
     for (int i = 0; i < simulation.numOfCells; i++)
     {
-        // std::cout<<i<<": "<< simulation.densityBuffer[i][0]<<", "<< simulation.densityBuffer[i][1]<<std::endl;;
         REQUIRE(simulation.densityBuffer[i][0] == test_data::densityBufferForMultipleParticles[i][0]);
         REQUIRE(simulation.densityBuffer[i][1] == 0.0);
     }
 }
+
 /**
  * @brief Fill out this test function by filling in the TODOs
  * Tests the calculation of the gravitational potential due to a single particle
@@ -176,7 +193,50 @@ TEST_CASE("Test potential function for single particle", "Potential Tests")
     }
 }
 
-TEST_CASE("Test ZERO", "Simulation Loop Tests")
+TEST_CASE("Test the gradient in a cube with 2*2*2 cells", "Gradient Tests")
+{
+    // One particle in the centre of the box with mass 0.01
+    particle::massSetter(test_data::mass);
+
+    // Declare simulation with particle setup, width, and number of cells.
+    // TODO: Simulation sim(...);
+    Simulation simulation(test_data::timeMax, test_data::timeStep, test_data::initParticlesMultiple, test_data::width, test_data::numOfCellsPerDim, test_data::expanFac);
+    simulation.accelerationCalculator(simulation.potentialRealPart);
+    REQUIRE(simulation.acceleration == test_data::zeroGradient); // in a 2*2*2 cube, the gradient should be 0 everywhere because of wrapping around
+}
+
+TEST_CASE("Test the gradient in a cube with 3*3*3 cells", "Gradient Tests")
+{
+    // One particle in the centre of the box with mass 0.01
+    particle::massSetter(test_data::mass);
+
+    // Declare simulation with particle setup, width, and number of cells.
+    // TODO: Simulation sim(...);
+    Simulation simulation(test_data::timeMax, test_data::timeStep, test_data::initParticlesMultiple, test_data::width, 3, test_data::expanFac);
+    simulation.accelerationCalculator(test_data::potentialRealPart_27);
+    REQUIRE(simulation.acceleration == test_data::gradient_27);
+}
+
+TEST_CASE("Test the updater in a cube with 2*2*2 cells", "Updater Tests")
+{
+    // One particle in the centre of the box with mass 0.01
+    particle::massSetter(test_data::mass);
+
+    // Declare simulation with particle setup, width, and number of cells.
+    // TODO: Simulation sim(...);
+    Simulation simulation(test_data::timeMax, test_data::timeStep, test_data::initParticlesAB, test_data::width, test_data::numOfCellsPerDim, test_data::expanFac);
+    simulation.particlesUpdater(test_data::acceleration);
+    simulation.particlesUpdater(test_data::acceleration);
+    for (int i = 0; i < 3; i++)
+    {
+        REQUIRE_THAT(simulation.particlesSimu.particleInfo[0].position[i], WithinRel(test_data::partilceAPos[i], 0.001));
+        REQUIRE_THAT(simulation.particlesSimu.particleInfo[0].velocity[i], WithinRel(test_data::partilceASpeed[i], 0.001));
+        REQUIRE_THAT(simulation.particlesSimu.particleInfo[1].position[i], WithinRel(test_data::partilceBPos[i], 0.001));
+        REQUIRE_THAT(simulation.particlesSimu.particleInfo[1].velocity[i], WithinRel(test_data::partilceBSpeed[i], 0.001));
+    }
+}
+
+TEST_CASE("Test simulation run", "Simulation Tests")
 {
     // One particle in the centre of the box with mass 0.01
     particle::massSetter(test_data::mass);
