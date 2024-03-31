5cc1389fb64d9b9ea2cc0499133b3210853b5b11
dan <xwj.harry@gmail.com>
2024-03-31 23:14:50 +0000

    refactor the initializtion method to improve efficiency

--- lib/particles.cpp
+++ lib/particles.cpp
@@ -3,21 +3,21 @@
 particles::particles(int numOfParticles, int seed)
 {
     srand(seed);
-    // this->particleInfo.clear();
+    this->particleInfo.resize(numOfParticles);
     for (int i = 0; i < numOfParticles; ++i)
     {
         particle particleX;
         particleX.positionSetter({rand() / (RAND_MAX + 1.0), rand() / (RAND_MAX + 1.0), rand() / (RAND_MAX + 1.0)});
-        this->particleInfo.push_back(particleX);
+        this->particleInfo[i] = particleX;
     }
 }
 
 particles::particles(std::vector<std::vector<double>> positions)
 {
-    // this->particleInfo.clear();
-    for (auto iter = positions.begin(); iter < positions.end(); iter++)
+    this->particleInfo.resize(positions.size());
+    for (int i = 0; i < positions.size(); i++)
     {
-        particle particleX(*iter);
-        this->particleInfo.push_back(particleX);
+        particle particleX(positions[i]);
+        this->particleInfo[i] = particleX;
     }
 }
